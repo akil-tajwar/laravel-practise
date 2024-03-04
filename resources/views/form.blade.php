6fe5ee2e7b7cdc9eeee472235e5834ce27a20ad5
Akil Tajwar <akiltajwar@gmail.com>
2024-03-05 01:57:42 +0600

    form validation

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -12,16 +12,40 @@
     <form action="{{url('/')}}/register" method="post">
         @csrf
         <div>
-            <label for="">Name</label>
-            <input type="text" name="name">
+            <label for="">Name</label><br>
+            <input type="text" name="name" value="{{old('name')}}">
+            <div>
+                @error('name')
+                    {{$message}}
+                @enderror
+            </div>
         </div>
         <div>
-            <label for="">Email</label>
-            <input type="email" name="email">
+            <label for="">Email</label><br>
+            <input type="email" name="email" value="{{old('email')}}">
+            <div>
+                @error('email')
+                    {{$message}}
+                @enderror
+            </div>
         </div>
         <div>
-            <label for="">Password</label>
+            <label for="">Password</label><br>
             <input type="password" name="password">
+            <div>
+                @error('password')
+                    {{$message}}
+                @enderror
+            </div>
+        </div>
+        <div>
+            <label for="">Confirm Password</label><br>
+            <input type="password" name="password_confirmation">
+            <div>
+                @error('password_confirmation')
+                    {{$message}}
+                @enderror
+            </div>
         </div>
         <button>Submit</button>
     </form>
